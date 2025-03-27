698b130888758cdfc4e88d0037b96ec75897299a
Lucia Ruiz <luruiz@itba.edu.ar>
2025-03-27 15:48:05 -0300

    added comments

--- mvp-0/modules/sim/fetch/fetch_tb.v
+++ mvp-0/modules/sim/fetch/fetch_tb.v
@@ -29,12 +29,14 @@ module fetch_tb;
         $dumpfile("tb_fetch.vcd");
         $dumpvars(0, fetch_tb);
 
+    // Verify that when resetn = 0 the PC stays in 0 and the instr stays NOP.
         #10;
         $display("PC=%d, Instruction =%h, resetn=%d, clk =%d",pc,instr,resetn,clk);
         
         #10;
         $display("PC=%d, Instruction =%h, resetn=%d, clk =%d",pc,instr,resetn,clk);
         
+    // Resetn = 1 so now the PC should increase with every clk posedge.
         resetn = 1;
 
         #10;
