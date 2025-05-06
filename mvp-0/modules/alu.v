116a17fe17b50448d91756c97ec57083be91d0dc
Jhertter <84042900+Jhertter@users.noreply.github.com>
2025-05-05 23:25:01 -0300

    Merge Request for wiring tests (#19)
    * Compatibilidad con decoder y pasaje a Amaranth
    
    * Converted to wiring.Module
    
    ---------
    
    Co-authored-by: cmeichtry <cmeichtry@itba.edu.ar>

--- mvp-0/modules/alu.v
+++ mvp-0/modules/alu.v
@@ -2,96 +2,68 @@ module alu(
     input wire [31:0] A, B,
     input wire [2:0] func3,
     input wire [6:0] func7,
-    input wire [6:0] opcode,
     input wire [4:0] shamt,
+
+    input wire isALUreg,
+    input wire isALUimm,
+    input wire isBranch,
+    input wire isJALR,
+    input wire isJAL,
+    input wire isAUIPC,
+    input wire isLUI,
+    input wire isLoad,
+    input wire isStore,
+
     output reg EQ, EQM, EQM_U,
     output reg [31:0] Q
+
+    
 );
 
 always @(*) 
 begin
-    case(opcode)
-    /* Immediate Instructions and Record Upload */
-        7'b0110111: //LUI
+        Q = 32'b0;
+
+        if (isALUreg || isALUimm) 
+        begin
+            case (func3)
+                3'b000: 
+                begin
+                    if (func7[5] && isALUreg)
+                        Q = A - B;
+                    else 
+                        Q = A + B;
+                end
+                3'b111: Q = A & B;
+                3'b110: Q = A | B;
+                3'b100: Q = A ^ B;
+                3'b001: Q = A << shamt;
+                3'b101: Q = (func7[5]) ? (A >>> shamt) : (A >> shamt);
+                3'b010: Q = ($signed(A) < $signed(B)) ? 32'b1 : 32'b0;
+                3'b011: Q = (A < B) ? 32'b1 : 32'b0;
+                default: Q = 32'b0;
+            endcase
+        end
+        else if (isAUIPC) 
+        begin
+            Q = A + B;
+        end
+        else if (isLUI) 
+        begin
             Q = B;
-        7'b0010111: //AUIPC
+        end
+        else if (isJAL || isJALR) 
+        begin
+            Q = A + 4;
+        end
+        else if (isLoad || isStore) 
+        begin
             Q = A + B;
-        7'b1101111: //JAL
-            Q = 0;
-        7'b1100111: //JALR
-            Q = A + B;
+        end
+        else 
+        begin
+            Q = 32'b0;
+        end
+    end
 
-        /* Jumping Instructions */
-        7'b1100011: //JUMP
-            case(func3)
-                3'b000: //BEQ
-                    Q = A == B;
-                3'b001: //BNQ
-                    Q = A != B;
-                3'b100: //BLT
-                    Q = $signed(A) < $signed(B);
-                3'b101: //BGE
-                    Q = $signed(A) >= $signed(B);
-                3'b110: //BLTU
-                    Q = A < B;
-                default: //BGEU
-                    Q = A >= B;
-            endcase
-
-        /* Load and Store Instructions */
-        7'b0000011:     //LOAD
-            Q = A + B;
-
-        7'b0100011:     //STORE
-            Q = A + B;
-
-        /* Immediate Instructions */
-        7'b0010011:
-            case(func3)
-                3'b000: //ADDI
-                    Q = A + B;
-                3'b010: //SLTI, Q = 1 if A<B
-                    Q = $signed(A) < $signed(B);
-                3'b011: //SLTI Unsigned, Q = 1 if A<B
-                    Q = A < B;
-                3'b100: //XORI
-                    Q = A ^ B;
-                3'b110: //ORI
-                    Q = A | B;
-                3'b111: //ANDI
-                    Q = A & B;
-                3'b001: //SHIFT LEFT LOGICAL IMMEDIATE (SLLI)
-                    Q = A << shamt;
-                default:
-                    Q = (func7[5]) ? (A >>> shamt) : (A >> shamt); //SRA or SRL
-            endcase
-
-        /* Operations */
-        7'b0110011:
-            case(func3)
-                3'b000:
-                    Q = (func7[5]) ? (A - B) : (A + B); //SUB or ADD
-                3'b010: //SLT, Q = 1 if A<B
-                    Q = A < B;
-                3'b011: //SLT Unsigned, Q = 1 if A<B
-                    Q = $signed(A) < $signed(B);
-                3'b100: //XOR
-                    Q = A ^ B;
-                3'b001: //SHIFT LEFT LOGICAL
-                    Q = A << shamt;
-                3'b101:
-                    Q = (func7[5]) ? (A >>> shamt) : (A >> shamt); //SRA or SRL
-                3'b110: //OR
-                    Q = A | B;
-                default: //AND
-                    Q = A & B;
-            endcase
-        default: //NOP
-            Q = 0;
-    endcase
-
-    EQ = A == B;
-    EQM = A > B;
-    EQM_U = A < B;
-end
 endmodule
