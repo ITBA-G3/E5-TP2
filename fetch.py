bb63e0a779d4f3b2a3ff25d9e561273f6d205b27
Lucia Ruiz <luruiz@itba.edu.ar>
2025-06-23 14:39:03 -0300

    prueba sencilla con una instr addi yendo por el pipeline

--- fetch.py
+++ fetch.py
@@ -20,11 +20,11 @@ class Fetch(wiring.Component):
         mem = Memory(width=32, depth=256, init=[
             0b0000000_00000_00001_000_00000_0000000,    # nop
             0x3e800093,    # addi x1, x0, 1000
-            0x7d008113,    # addi x2, x1, 2000
-            0xb1e10193,    # addi x3, x2, -1250
-            0x00518113,    # addi x2, x3, 5
+            # 0x7d008113,    # addi x2, x1, 2000
+            # 0xb1e10193,    # addi x3, x2, -1250
+            # 0x00518113,    # addi x2, x3, 5
 
-        ] + [0b0000000_00000_00001_000_00000_0000000]*(256-5)) # NOP pad the rest
+        ] + [0b0000000_00000_00001_000_00000_0000000]*(256-2)) # NOP pad the rest
 
 
         rdport = mem.read_port()
