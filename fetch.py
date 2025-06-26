f32757ba7f101e82b4aa32be2e82289b9cdfb266
Agustin Fisher <afisher@itba.edu.ar>
2025-06-25 21:25:46 -0300

    bram

--- fetch.py
+++ fetch.py
@@ -16,15 +16,19 @@ class Fetch(wiring.Component):
     def elaborate(self, platform):
         m = Module()
 
-        # 256-word 32-bit wide instr mem
-        mem = Memory(width=32, depth=256, init=[
+        program = [
             0b0000000_00000_00001_000_00000_0000000,    # nop
             0x3e800093,    # addi x1, x0, 1000
             0x7d008113,    # addi x2, x1, 2000
             # 0xb1e10193,    # addi x3, x2, -1250
             # 0x00518113,    # addi x2, x3, 5
 
-        ] + [0b0000000_00000_00001_000_00000_0000000]*(256-3)) # NOP pad the rest
+        ]
+
+        # 256-word 32-bit wide instr mem
+        mem = Memory(width=32, depth=256, init=program +
+        [0b0000000_00000_00001_000_00000_0000000]*(256-len(program)),
+        attrs={"ram_style": "block"}) # NOP pad the rest
 
 
         rdport = mem.read_port()
